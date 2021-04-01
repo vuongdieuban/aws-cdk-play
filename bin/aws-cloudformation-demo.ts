374773ac049e1860025204371943c36a6f03ea82
ban vuong <vuongdieuban@gmail.com>
2021-03-31 20:06:05 -0600

    add stage construct comment in main file

--- bin/aws-cloudformation-demo.ts
+++ bin/aws-cloudformation-demo.ts
@@ -4,6 +4,9 @@ import { EcsFargateStack } from '../lib/ecs-fargate-stack';
 import { LambdaHandlerStack } from '../lib/lambda-handler-stack';
 import { GreetingStack } from '../lib/ecs-app-mesh-stack';
 
+// To deploy stack together, put stage into Stage (Stage construct aws cdk)
+// Stage could be Staging, Production,....
+// each stage can consist of different stack.
 const app = new cdk.App();
 new LambdaHandlerStack(app, 'LambdaHandlerStack');
 new EcsFargateStack(app, 'EcsFargateStack');
