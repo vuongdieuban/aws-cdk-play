4410cf5039f8255d1c832e391cc092eafd73b407
ban vuong <vuongdieuban@gmail.com>
2021-03-31 20:05:23 -0600

    put lambda inside vpc

--- lib/lambda-handler-stack.ts
+++ lib/lambda-handler-stack.ts
@@ -1,19 +1,30 @@
-import { Stack, StackProps, App } from '@aws-cdk/core';
+import { Stack, StackProps, App, CfnOutput } from '@aws-cdk/core';
 import { Function as LambdaFunction, Runtime, Code } from '@aws-cdk/aws-lambda';
 import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import { SubnetType, Vpc } from '@aws-cdk/aws-ec2';
 
 export class LambdaHandlerStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const vpc = new Vpc(this, 'GreetingVpc', {
+      maxAzs: 1,
+    });
+
     const helloHandler = new LambdaFunction(this, 'HelloHandler', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset('applications/lambda-handlers/dist'),
       handler: 'hello-world.helloHandler', // hello-world.js file, helloHandler function,
+      vpc,
     });
 
-    new LambdaRestApi(this, 'Endpoint', {
+    const restApi = new LambdaRestApi(this, 'Endpoint', {
       handler: helloHandler,
+    });
+
+    new CfnOutput(this, 'RestApi', {
+      exportName: 'rest-api-dns',
+      value: restApi.url || 'no-url-found',
     });
   }
 }
