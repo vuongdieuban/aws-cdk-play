db11aba42676416760b5fc4b9e4aaed950c0723b
ban vuong <vuongdieuban@gmail.com>
2021-04-10 16:14:23 -0600

    add name server

--- applications/nodejs-apps/name/index.ts
+++ applications/nodejs-apps/name/index.ts
@@ -5,8 +5,18 @@ const app = express();
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+const generateRandomNumer = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
+const names = ['Ban', 'Stephen', 'David', 'Sanjith'];
+
 app.get('/', (req, res) => {
-  res.send('hello world from express app');
+  const nameIndex = generateRandomNumer(names.length);
+  const name = names[nameIndex];
+  res.json({
+    name,
+  });
 });
 
 app.listen(PORT, HOST);
