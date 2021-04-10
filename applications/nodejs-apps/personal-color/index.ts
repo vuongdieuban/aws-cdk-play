122a25c0d67a7f09eaa681d80819524d18ba70fd
ban vuong <vuongdieuban@gmail.com>
2021-04-10 16:24:39 -0600

    favourite color server

--- applications/nodejs-apps/personal-color/index.ts
+++ applications/nodejs-apps/personal-color/index.ts
@@ -1,12 +1,42 @@
 import * as express from 'express';
+import axios from 'axios';
+
+interface Name {
+  name: string;
+}
+
+interface Color {
+  color: string;
+  version: string;
+}
 
 const app = express();
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-app.get('/', (req, res) => {
-  res.send('hello world from express app');
+const NAME_URL = process.env.NAME_URL;
+const COLOR_URL = process.env.COLOR_URL;
+
+app.get('/', async (req, res) => {
+  if (!NAME_URL || !COLOR_URL) {
+    console.log('Name_Url', NAME_URL);
+    console.log('Color_Url', COLOR_URL);
+    throw new Error('Missing environment variable');
+  }
+
+  const namePromise = axios.get<Name>(NAME_URL);
+  const colorPromise = axios.get<Color>(COLOR_URL);
+
+  const [{ data: nameData }, { data: colorData }] = await Promise.all([namePromise, colorPromise]);
+
+  const favoriteColor = `${nameData.name} likes ${colorData.color}`;
+
+  res.json({
+    colorData,
+    nameData,
+    favoriteColor,
+  });
 });
 
 app.listen(PORT, HOST);
