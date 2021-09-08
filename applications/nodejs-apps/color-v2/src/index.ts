2a0b73a48d937980cc3305eda0d72fdc326e3e59
ban vuong <vuongdieuban@gmail.com>
2021-09-07 23:00:35 -0600

    add version to health

--- applications/nodejs-apps/color-v2/src/index.ts
+++ applications/nodejs-apps/color-v2/src/index.ts
@@ -1,5 +1,11 @@
 import * as express from 'express';
 
+function sleep(timeMs: number) {
+  return new Promise(res => {
+    setTimeout(res, timeMs);
+  });
+}
+
 const app = express();
 
 const PORT = 3000;
@@ -11,12 +17,17 @@ const generateRandomNumer = (max: number) => {
 const colors = ['red', 'blue', 'green', 'black'];
 
 app.get('/health', (req, res) => {
-  res.json(true);
+  res.json({
+    health: true,
+    version: 'v2',
+  });
 });
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const colorIndex = generateRandomNumer(colors.length);
   const chosenColor = colors[colorIndex];
+  await sleep(60 * 1000);
+  console.log('-----About to response-----');
   res.json({
     color: chosenColor.toUpperCase(),
     version: 'v2',
